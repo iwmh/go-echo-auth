3967d8feaa4723bf156aca4123813b8b3061cd0e
iwmh <hiroshiplusone@gmail.com>
2019-06-19 22:54:07 +0900

    Playing around cookie and session

--- server.go
+++ server.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"github.com/gorilla/sessions"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
 	"net/http"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/login", func(c echo.Context) error {
+		sess, _ := session.Get("ses_echo", c)
+		sess.Options = &sessions.Options{
+			Path:     "/",
+			MaxAge:   86400 * 7,
+			HttpOnly: true,
+		}
+		sess.Values["foo"] = "bar"
+		sess.Save(c.Request(), c.Response())
+		return c.NoContent(http.StatusOK)
+	})
+	type Cookie struct {
+		Name  string `json:"name"`
+		Value string `json:"value"`
+	}
+	e.GET("/cookie", func(c echo.Context) error {
+		cookie, err := c.Cookie("ses_echo")
+		if err != nil {
+			return err
+		}
+		co := &Cookie{
+			Name:  cookie.Name,
+			Value: cookie.Value,
+		}
+		return c.JSON(http.StatusOK, co)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
